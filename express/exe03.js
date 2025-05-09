5679d20efb58a6e601cfc8539f8651f11b93f4ad
diogo <diogo.lima.flooxai@gmail.com>
2025-05-09 10:45:20 -0300

    feat: add Route method

--- express/exe03.js
+++ express/exe03.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const server = express();
 
-server.use('/api', function (req, res, next) {
+server.use(function (req, res, next) {
   console.log('Inicio...');
   next();
   console.log('Fim...');  
 })
 
-server.use('/api', function (req, res) {
+server.use(function (req, res) {
    console.log('Resposta...');  
    res.send('<h1>API</h1>');
 })
