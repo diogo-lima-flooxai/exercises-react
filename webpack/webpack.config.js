bcf3dbf962f9aa54837da10f9b5421956240154d
diogo <diogo.lima.flooxai@gmail.com>
2025-05-12 09:38:48 -0300

    feat: add the application for production and development
    updating minification versions

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,27 +1,39 @@
-const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const modoDev = process.env.NODE_ENV !== 'production';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    optimization: {
+        minimize: !modoDev,
+        minimizer: [
+            new TerserPlugin({
+                parallel: true
+            }),
+            new CssMinimizerPlugin()
+        ]
+    },
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'estilo.css'
-        }),
+        })
     ],
     module: {
-        rules: [{
-            test: /\.s?[ac]ss$/,
-            use: [
-                MiniCssExtractPlugin.loader, // Extrai o CSS para um arquivo separado
-                //'style-loader', // Adiona o CSS na DOM
-                'css-loader', // Interpreta @import, url()
-                'sass-loader' // Compila o Sass para CSS
-            ]
-        }]
+        rules: [
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
     }
-}
+};
