73af464dcf2d029cbda95011022ef0fbd8989b9d
diogo <diogo.lima.flooxai@gmail.com>
2025-05-12 09:51:21 -0300

    feat: add webserver and upload images

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,6 +2,7 @@ const modoDev = process.env.NODE_ENV !== 'production';
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     mode: modoDev ? 'development' : 'production',
@@ -9,6 +10,13 @@ module.exports = {
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        compress: true,
+        port: 9000
     },
     optimization: {
         minimize: !modoDev,
@@ -30,9 +38,12 @@ module.exports = {
                 test: /\.s?[ac]ss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    'css-loader',
+                    'css-loader', 
                     'sass-loader'
                 ]
+            }, {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
             }
         ]
     }
